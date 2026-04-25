633b19d5dc81e21074c3e89875e3c296efdfae9d
ZoniD <deba0001@stud.ek.dk>
2026-04-25 19:50:55 +0200

    first push to see in github

---
+++ src/test/java/dk/ek/workly/WorklyApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.workly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
